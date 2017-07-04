0eb450180347aa23b6d9a2d11356a74186f3df6c
dvarela <dvarela@skytouchtechnology.com>
2017-07-04 10:14:50 -0600

    added some code

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'active_record'
+
+# CREATE DATABASE  rubyschool;
+# CREATE TABLE usuarios (
+#    id SERIAL NOT NULL PRIMARY KEY,
+#    nombre TEXT NOT NULL DEFAULT '',
+#    apellidos TEXT NOT NULL DEFAULT ''
+#);
+
+# Change the following to reflect your database settings
+ActiveRecord::Base.establish_connection(
+  adapter:  'postgresql', # or 'postgresql' or 'sqlite3' or 'oracle_enhanced'
+  host:     'localhost',
+  database: 'rubyschool',
+  username: 'dan',
+  password: ''
+)
+
+class Usuario < ActiveRecord::Base
+end 
